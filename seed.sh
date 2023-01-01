1044e3c81b2528aa44392e710192683618d5e17a
sneaky-potato <ashwanikamal.im421@gmail.com>
2023-01-01 14:20:15 +0530

    Add use case for .env not present in seed script

--- seed.sh
+++ seed.sh
@@ -10,39 +10,52 @@ NC='\033[0m'
 
 # Functions for printing and logging information on console
 function log {
-  	echo -e "${PRIMARY}${BRAND} ${LOG}$1${NC} ${INFO}$2${NC}"
+  	echo -e "${PRIMARY}${BRAND} ${LOG}$1${NC}"
 }
 
 function success {
   	echo -e "${PRIMARY}${BRAND} ${SUCCESS}$1${NC}"
 }
 
+function info {
+  	echo -e "${PRIMARY}${BRAND} ${INFO}$1${NC}"
+}
+
+
 # Checking for .env file 
-if [ ! -f .env ]; then
-    log ".env not found in root directory"
+if [ -f .env ]; then
+    log "::found .env in root directory::"
+
+    if grep -qv '^#' .env; then
+        log "::loading variables from .env::"
+        export $(grep -v '^#' .env | xargs)
+    fi 
+fi
+
+if [[ ! -v MEILI_MASTER_KEY ]]; then
+    info "MEILI_MASTER_KEY is not set"
+    exit
+
+elif [[ -z "$MEILI_MASTER_KEY" ]]; then
+    info "MEILI_MASTER_KEY is set to the empty string"
+    exit
+
 else
-    log "loaded .env"
-    export $(grep -v '^#' .env | xargs)
-
-    if [[ ! -v MEILI_MASTER_KEY ]]; then
-        log "MEILI_MASTER_KEY is not set"
-        exit
-
-    elif [[ -z "$MEILI_MASTER_KEY" ]]; then
-        log "MEILI_MASTER_KEY is set to the empty string"
-        exit
-
-    else
-        success "Using this master key: $MEILI_MASTER_KEY"
-    fi
+    success "> Using this master key: "
+    info $MEILI_MASTER_KEY
 fi
 
 
 if [ ! -f meilisearch ]
 then
-    log "meilisearch executable not found. Dowloading from source"
+    log "::meilisearch executable not found. Dowloading from source::"
     curl -L https://install.meilisearch.com | sh
 fi
 
 # TODO
 # prompt user to add data and start backend instance
+
+success "Instance ready to launch with the master key"
+success "Launching ./meilisearch"
+
+./meilisearch 
